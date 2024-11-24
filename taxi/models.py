4403f0b11f42b39a4387beaca16bcebdc1b36568
Viktoria Kalinina <viktoriakalinina@orderberry.de>
2024-11-24 17:07:01 +0100

    Solution

--- taxi/models.py
+++ taxi/models.py
@@ -1,14 +1,27 @@
+
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 
 
 class Manufacturer(models.Model):
     name = models.CharField(max_length=255, unique=True)
     country = models.CharField(max_length=255)
 
+    def __str__(self):
+        return f"{self.name} ({self.country})"
+
+    class Meta:
+        ordering = ("name", )
+
 
 class Driver(AbstractUser):
     license_number = models.CharField(max_length=255, unique=True)
+
+    def get_absolute_url(self):
+        return reverse(
+            "taxi:driver-detail", args=[str(self.id)]
+        )
 
 
 class Car(models.Model):
@@ -17,3 +30,9 @@ class Car(models.Model):
         Manufacturer, on_delete=models.CASCADE, related_name="cars"
     )
     drivers = models.ManyToManyField(Driver, related_name="cars")
+
+    def get_absolute_url(self):
+        return reverse(
+            "taxi:car-detail",
+            args=[str(self.pk)]
+        )
--- taxi/views.py
+++ taxi/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 from taxi.models import Driver, Car, Manufacturer
-
+from django.views.generic import ListView, DetailView
 
 def index(request):
     """View function for the home page of the site."""
@@ -12,4 +12,35 @@ def index(request):
         "num_manufacturers": Manufacturer.objects.count(),
     }
 
-    return render(request, "taxi/index.html", context=context)
+    return render(
+        request,
+        "taxi/index.html",
+        context=context)
+
+class ManufacturerListView(ListView):
+    model = Manufacturer
+    template_name = "taxi/manufacturer_list.html"
+    queryset = Manufacturer.objects.all()
+    paginate_by = 5
+
+class CarListView(ListView):
+    model = Car
+    template_name = "taxi/car_list.html"
+    queryset = (
+        Car.objects.all()
+        .select_related("manufacturer")
+        .prefetch_related("drivers"))
+    paginate_by = 5
+
+class CarDetailView(DetailView):
+    model = Car
+    template_name = "taxi/car_detail.html"
+
+class DriverListView(ListView):
+    model = Driver
+    template_name = "taxi/driver_list.html"
+    paginate_by = 5
+
+class DriverDetailView(DetailView):
+    model = Driver
+    template_name = "taxi/driver_detail.html"
